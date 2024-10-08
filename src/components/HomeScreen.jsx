7475e8d4e789e4d0067c6a8b86b5e1d21a02555a
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-10-08 18:53:07 +0100

    added to the code.

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,9 @@
+import "../styles/HomeScreen.css";
+
+function HomeScreen() {
+  <h2>Quizzical</h2>;
+  <p>Take part in our fun quiz!</p>;
+  <button>Start Quiz</button>;
+}
+
+export default HomeScreen;
