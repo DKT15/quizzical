733d37f9d0167b9ea11d2f21ea21d7a8653b1ab7
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-10-08 18:56:06 +0100

    added to the code.

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,9 +1,11 @@
 import "../styles/HomeScreen.css";
 
 function HomeScreen() {
-  <h2>Quizzical</h2>;
-  <p>Take part in our fun quiz!</p>;
-  <button>Start Quiz</button>;
+  <h2 className="home-title">Quizzical</h2>;
+  <p className="home-text">Take part in our fun quiz!</p>;
+  <a href="./Quiz.jsx" className="quiz-btn">
+    Start Quiz
+  </a>;
 }
 
 export default HomeScreen;
