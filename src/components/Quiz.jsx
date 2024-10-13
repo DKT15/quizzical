7483728755b196c59425fa6d8236ce30440426e7
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-10-13 16:41:18 +0100

    added to the code.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,3 +3,15 @@ import "../styles/Quiz.css";
 function Quiz() {
   return <></>;
 }
+
+/* 
+
+Pull in 5 questions and options from API using a side effect as the API is outside of React.
+Style the 5 questions ans put them in their own section. Props will be used to do this.
+When the use selects an answer let the button fade. (UseState)
+Add a check answers button below the code and have it check the users selected values against the correct value. (useState)
+If the answer is correct, highlight the users selection green. If the user selection is false, highlight the users selection red and highlight the correct answer in green.
+Display below how many questions the user got correct based in their answers.
+Beside the result, have a button that allows the user to play again and refresh the app.
+
+*/
