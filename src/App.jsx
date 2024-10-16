52b4a871d14eb2ffcb5be883dbdebef27b2bd67c
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-10-16 20:43:01 +0100

    added to the code.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import React, { useState } from "react";
 import HomeScreen from "./components/HomeScreen.jsx";
 
 function App() {
-  const [questions, setQuestions] = useState();
+  const [questionsData, setQuestionsData] = useState({});
+
+  React.useEffect(() => {
+    fetch(
+      "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple"
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  });
 
   return (
     <>
