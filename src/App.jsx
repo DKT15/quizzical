daba4f8da778b512ccffd399e70a7e2a7e509f6a
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-11-15 17:54:09 +0000

    added to the code.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,6 @@ import React from "react";
 import HomeScreen from "./components/HomeScreen.jsx";
 
 function App() {
-  const [questionsData, setQuestionsData] = React.useState({});
-
-  // API is outside of React, therefore useEffect is needed.
-  React.useEffect(() => {
-    fetch(
-      "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple"
-    )
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  });
-
   return (
     <>
       <HomeScreen />
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,27 @@
 import "../styles/Quiz.css";
+import React from "react";
+
+//Keeping state local no need to pass props down here.
 
 function Quiz() {
+  const [questionsData, setQuestionsData] = React.useState({});
+
+  // API is outside of React, therefore useEffect is needed.
+  React.useEffect(() => {
+    fetch(
+      "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple"
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  });
+
   return <></>;
 }
 
 /* 
 
 Pull in 5 questions and options from API using a side effect as the API is outside of React.
-Style the 5 questions ans put them in their own section. Props will be used to do this.
+Style the 5 questions and put them in their own section. Props will be used to do this.
 When the use selects an answer let the button fade. (UseState)
 Add a check answers button below the code and have it check the users selected values against the correct value. (useState)
 If the answer is correct, highlight the users selection green. If the user selection is false, highlight the users selection red and highlight the correct answer in green.
