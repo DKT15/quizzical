504d3ce1d13992e3c51c9c29aaaeb679a9e5dab9
Danté Kellman-Thompson <dante.kellman@icloud.com>
2025-03-05 17:38:15 +0000

    added to the code.

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -4,25 +4,24 @@ import React from "react";
 //Keeping state local no need to pass props down here.
 
 export function Quiz() {
-  // const [questionsData, setQuestionsData] = React.useState({});
+  const [questionsData, setQuestionsData] = React.useState([]);
 
-  // // API is outside of React, therefore useEffect is needed.
-  // React.useEffect(() => {
-  //   fetch(
-  //     "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple"
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => setQuestionsData(data));
-  // });
+  React.useEffect(() => {
+    fetch(
+      "https://opentdb.com/api.php?amount=5&category=21&difficulty=medium&type=multiple"
+    )
+      .then((res) => res.json())
+      .then((data) => setQuestionsData(data));
+  }, []);
+
+  function getQuestionsData() {}
+
+  function checkAnswers() {}
 
   return (
     <>
-      {/* testing code out */}
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
-      <h1>Test</h1>
+      <section className="questions-data">{getQuestionsData}</section>
+      <button onClick={checkAnswers}>Check answers</button>
     </>
   );
 }
@@ -31,12 +30,16 @@ export default Quiz;
 
 /* 
 
-Pull in 5 questions and options from API using a side effect as the API is outside of React.
-Style the 5 questions.
-When the user selects an answer let the button fade. (UseState) // note - might be best to do this in App.jsx. 
-Add a check answers button below the code and have it check the users selected values against the correct value. (useState)
-If the answer is correct, highlight the users selection green. If the user selection is false, highlight the users selection red and highlight the correct answer in green.
-Display below how many questions the user got correct based in their answers.
-Beside the result, have a button that allows the user to play again and refresh the app.
+- Pull in 5 questions and options from API using a side effect as the API is outside of React.
+- Render the five questions and the multiple choice answers to the page from the data recieved from the API.
+- When the user selects an answer highlight their option. They are allowed to change their answer but do not allow them to choose more than one answer.
+- Save the users guessed answers and compare them to the correct answers in the api.
+- When the user hits the checkAnswers button allow the state to change so that if the users selection is right it highlights green 
+and if the user is wrong highlight their option red and the correct answer green.
+
+- In doing this the check answers button will need to be replaced for UX.
+- Display below the five questions, how many answers the user got correct.
+- Besides the above allow the user to hit play again and reset the state of the game. This will load new or the same questions.
+- Create a new function or variable that resets the game.
 
 */
